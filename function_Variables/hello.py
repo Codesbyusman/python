a4a8f5b08317f9ce09186cf04faf35c16697db8e
Muhammad Usman Shahid <codesbyusman@gmail.com>
2022-07-25 00:18:50 +0500

    more ---

--- function_Variables/hello.py
+++ function_Variables/hello.py
@@ -1,6 +1,10 @@
 # comments by the # symbol
 
 # taking the input from the user
+from tracemalloc import start
+from turtle import begin_fill
+
+
 name = input("What is your name : " )
 
 """
@@ -8,9 +12,23 @@ multiline comment
 
 """
 # printing the output
-print("hello , " , name) # single line
+print("hello ," , name) # single line
 
 # printing the output multiple lines
-print("hello , " , end = "")
+print("hello , " , end = "") # removing the default end='\n'
 print(name)
+
+
+print("hello ," , name, sep = " ::: i am sep ::: " ) # removing the default sep(separator to -)
+
+
+# using the curly braces to print the output the name
+# the format strings
+print(f"hello , {name} " )
+
+
+# strings data type power
+name = name.strip() # removing the spaces from the start and end of the string
+print(name)
+
 
