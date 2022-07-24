1d6b496168874fa3b514b867876d4e9dcaa6f84e
Muhammad Usman Shahid <codesbyusman@gmail.com>
2022-07-25 00:08:57 +0500

    hello

--- function_Variables/hello.py
+++ function_Variables/hello.py
@@ -1,3 +1,16 @@
-name = input("What is your name : ")
+# comments by the # symbol
 
-print("hello , " , name)
+# taking the input from the user
+name = input("What is your name : " )
+
+"""
+multiline comment
+
+"""
+# printing the output
+print("hello , " , name) # single line
+
+# printing the output multiple lines
+print("hello , " , end = "")
+print(name)
+
