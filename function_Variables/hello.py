01841d6092331315b758e663059e52ef0828cb19
Muhammad Usman Shahid <codesbyusman@gmail.com>
2022-07-25 00:31:41 +0500

    more on variable

--- function_Variables/hello.py
+++ function_Variables/hello.py
@@ -31,4 +31,20 @@ print(f"hello , {name} " )
 name = name.strip() # removing the spaces from the start and end of the string
 print(name)
 
+# capitalizing the first letter of the string
+print(name.capitalize())
+#  capitalizing the first letter of the string and all the other letters in the string
+print(name.title())
+
+# doing all one time
+name.capitalize().title()
+
+# can be done on input 
+input("Press Enter to continue...").title().capitalize()
+
+# spliting the string 
+first , last = name.split(" ")
+
+print(first)
+print(last)
 
